0a1199737024468720afbbb1e15bcb2ae4bd4edb
sanserif28 <sanserif28@gmail.com>
2026-08-02 00:35:25 +0900

    refactor(part-5-1): defer reconcile index to part-5-2

--- src/main/resources/lua/issue.lua
+++ src/main/resources/lua/issue.lua
@@ -1,5 +1,5 @@
--- KEYS: stock, users (set), sold_out, reconcile_index (zset)
--- ARGV: userId, soldOutTtlSeconds, issuedAtMs, couponId
+-- KEYS: stock, users (set), sold_out
+-- ARGV: userId, soldOutTtlSeconds
 -- 반환: 1=성공, 0=매진, -1=중복 발급
 
 if redis.call('SISMEMBER', KEYS[2], ARGV[1]) == 1 then
@@ -13,8 +13,6 @@ end
 
 redis.call('DECR', KEYS[1])
 redis.call('SADD', KEYS[2], ARGV[1])
--- 대사 sliding window 인덱스: 쿠폰별 최신 발급 시각을 score 로 (member 는 couponId, ZADD 가 갱신).
-redis.call('ZADD', KEYS[4], ARGV[3], ARGV[4])
 
 if remaining == 1 then
   redis.call('SET', KEYS[3], '1', 'EX', ARGV[2])
